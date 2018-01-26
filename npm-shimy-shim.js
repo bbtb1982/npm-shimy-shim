bd6c6df77e4f1d9afd5e9f544fcae8a494ddcfbf
Brandon Richards <bbtb1982@gmail.com>
2018-01-25 17:48:28 -0700

    add the stuffs

--- npm-shimy-shim.js
+++ npm-shimy-shim.js
@@ -1,13 +1,16 @@
-var npmShimyShim = (function() {
+;(function (window) {
+  'use strict';
+
   var question = "Who's the baddest mofo low down around this town?";
   var answer = "SHO' NUFF";
   var image = "http://3.bp.blogspot.com/_EG5tz5Spz6E/SRRigelz9QI/AAAAAAAABvk/aypUBeu2z-M/s320/shogun+of+harlem.jpg"
-  var poster= "https://i.pinimg.com/736x/12/65/1b/12651b5bb8b2836ccc3f6add7cc533b2--harlem-character-art.jpg"
+  var poster = "https://i.pinimg.com/736x/12/65/1b/12651b5bb8b2836ccc3f6add7cc533b2--harlem-character-art.jpg"
 
-  return {
+  window['npm-shimy-shim'] = {
     question: question,
     answer: answer,
     image: image,
     poster: poster,
-  }
-}());
+  };
+})(window);
+
